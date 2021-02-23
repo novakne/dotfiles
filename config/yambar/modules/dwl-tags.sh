b00ad1ab65385b74523cbe9bc2af0255d77d4b1d
novakne <hmachet@zoho.com>
2021-02-23 08:49:34 +0100

    yambar: clean modules

--- config/yambar/modules/dwl-tags.sh
+++ config/yambar/modules/dwl-tags.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 #
-# Display dwl tags in yambar
-#     - Requirements: inotifywait ( 'inotify-tools' on arch )
+# dwl-tags.sh - display dwl tags
+#
+# USAGE: dwl-tags.sh 1
+#
+# REQUIREMENTS:
+#  - inotifywait ( 'inotify-tools' on arch )
+#  - 2021/02/22 - dwl pull request:
+#    'Interface to display tag information on status bar #91'
+#    https://github.com/djpohly/dwl/pull/91
+#
+# TAGS:
+#  Name      Type    Return
+#  -------------------------------------
+#  {dwltag}  string  dwl tags name/state
+#
+# Exemple configuration:
+#  - script:
+#      path: /absolute/path/to/dwl-tags.sh
+#      args: [1]
+#      content: {string: {text: "{dwltag}"}}
 
-# Use in yambar like this
-#     - script:
-#         path: "/absolute/path/to/dwl-tags.sh"
-#         args: [1]
-#         content: {string: {text: "{dwltag}"}}
 
-
+# Variables
 declare titleline tagline title taginfo isactive ctags mtags layout
+declare symbol_occupied_pre symbol_occupied_post symbol_focused_pre symbol_focused_post
 declare -a tags name
 readonly fname=/tmp/dwltags-"$WAYLAND_DISPLAY"
 
@@ -37,38 +51,61 @@ while true; do
 
   tags=( "1" "2" "3" "4" "5" "6" "7" "8" "9" )
 
-  # If you want to add a name to tags
-  # if there is no name, number are used
-  # Comment the next line to just use number
-  name=( "" "" "" )
+  # Name of tag (optional)
+  # If there is no name, number are used
+  #
+  # Example:
+  #  name=( "" "" "" "Media" )
+  #  -> return "" "" "" "Media" 5 6 7 8 9)
+  name=()
+
+  # Symbol for occupied tags
+  #
+  # Format: "{symbol_occupied_pre}{TAGNAME}{symbol_occupied_post}"
+  # You can leave one empty if you don't want to surround the TAGNAME
+  symbol_occupied_pre=""
+  symbol_occupied_post="."
+
+  # Symbol for the focused tag
+  #
+  # Format: "{symbol_focused_pre}{TAGNAME}{symbol_focused_post}"
+  # You can leave one empty if you don't want to surround the TAGNAME
+  symbol_focused_pre="[ "
+  symbol_focused_post=" ]"
 
   for i in {0..8}; do
     mask=$((1<<i))
+    n="$((tags[i]++))"
 
     # Occupied
     if (( "${ctags}" & mask )); then
-      n="$((tags[i]++))."
-      tags[i]="${name[i]:-${n}}"
+      name[i]="${symbol_occupied_pre}${name[i]:-${n}}${symbol_occupied_post}"
+      tags[i]="${name[i]}"
     else
-      n="$((tags[i]++))"
       tags[i]="${name[i]:-${n}}"
     fi
     
     # Focused
     if (( "${mtags}" & mask )); then
-      tags[i]="[ ${name[i]:-${n}} ]"
+      tags[i]="${symbol_focused_pre}${name[i]:-${n}}${symbol_focused_post}"
     else
       tags[i]=" ${name[i]:-${n}} "
     fi
 
   done
-
+  
+  # Format send to yambar
+  #
+  # tags[*]  - array of tags name
+  # layout   - dwl layout (tile, floating, monocle) (optional)
+  # title    - name of the focused client (optional)
   fmt="${tags[*]} ${layout} [ ${title:-1:50} ]" # Limit title to 50 caracters
 
   printf -- '%s\n' "dwltag|string|${fmt}"
-  printf -- '%s\n' "" # Need this so tags are updated
+  printf -- '%s\n' ""
 
 done
 
 unset -v titleline tagline title taginfo isactive ctags mtags layout 
+unset -v symbol_occupied_pre symbol_occupied_post symbol_focused_pre symbol_focused_post
 unset -v tags name
--- config/yambar/modules/pacman.sh
+++ config/yambar/modules/pacman.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
 #
-# Display number of packages update available
-# by default check every hour
+# pacman.sh - display number of packages update available
+#             by default check every hour
 #
-# [TAGS]
-# {pacman}        pacman packages
-# {aur}           manually installed packages (e.g. aur or local repo)
-# {pkg}           sum of both
+# USAGE: pacman.sh
 #
-# Simple exemple to use in yambar config:
-#     - script:
-#         path: "/absolute/path/to/pacman.sh"
-#         args: [] 
-#         content: { string: { text: " {pacman} + {aur} = {pkg}" } }
+# TAGS:
+#  Name      Type    Return
+#  -------------------------------------------
+#  {pacman}  string  number of pacman packages
+#  {aur}     string  noumber of aur packages
+#  {pkg}     string  sum of both
+#
+# Exemple configuration:
+#  - script:
+#    path: /absolute/path/to/pacman.sh
+#    args: [] 
+#    content: { string: { text: " {pacman} + {aur} = {pkg}" } }
 
 
-declare interval no_update aur_manager pacman_num aur_num pkg_num
+declare interval no_update aur_helper pacman_num aur_num pkg_num
+
+# Error message in STDERR
+_err() {
+  printf -- '%s\n' "[$(date +'%Y-%m-%d %H:%M:%S')]: $*" >&2
+}
+
 
 while true; do
-  # Change interval here
-  # NUMBER[SUFFIXE]...
+  # Change interval
+  # NUMBER[SUFFIXE]
   # Possible suffix:
-  #     "s" - seconds "m" - minutes "h" - hours "d" - days 
+  #  "s" seconds / "m" minutes / "h" hours / "d" days 
   interval="1h"
   
   # Change the message you want when there is 0 update
@@ -29,12 +39,18 @@ while true; do
   # (e.g. no_update="")
   no_update=""
   
-  # Change with your aur manager
-  aur_manager="paru"
+  # Change your aur manager
+  aur_helper="paru"
 
   # Get number of packages to update
   pacman_num=$(checkupdates | wc -l)
-  aur_num=$("${aur_manager}" -Qmu | wc -l)
+
+  if ! hash "${aur_helper}" >/dev/null 2>&1; then
+    _err "aur helper not found, change it in the script"
+  else
+    aur_num=$("${aur_helper}" -Qmu | wc -l)
+  fi
+  
   pkg_num=$(( pacman_num + aur_num ))
 
   # Only display one if there is no update and multiple tags set
@@ -58,5 +74,6 @@ while true; do
 
 done
 
-unset -v interval no_update aur_manager pacman_num aur_num pkg_num
+unset -v interval no_update aur_helper pacman_num aur_num pkg_num
+unset -f _err
 
