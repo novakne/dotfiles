cec3fdfd1054b0b4671d1ee8dff688187edbf18e
novakne <hmachet@zoho.com>
2021-02-24 11:19:32 +0100

    yambar: get the pacman script from master

--- config/yambar/modules/pacman.sh
+++ config/yambar/modules/pacman.sh
@@ -6,25 +6,42 @@
 # USAGE: pacman.sh
 #
 # TAGS:
-#  Name      Type    Return
+#  Name      Type  Return
 #  -------------------------------------------
-#  {pacman}  string  number of pacman packages
-#  {aur}     string  noumber of aur packages
-#  {pkg}     string  sum of both
+#  {pacman}  int   number of pacman packages
+#  {aur}     int   number of aur packages
+#  {pkg}     int   sum of both
 #
-# Exemple configuration:
+# Exemples configuration:
 #  - script:
-#    path: /absolute/path/to/pacman.sh
-#    args: [] 
-#    content: { string: { text: " {pacman} + {aur} = {pkg}" } }
+#      path: /absolute/path/to/pacman.sh
+#      args: [] 
+#      content: { string: { text: "{pacman} + {aur} = {pkg}" } }
+#
+# To display a message when there is no update:
+#  - script:
+#      path: /absolute/path/to/pacman.sh
+#      args: [] 
+#      content:
+#        map:
+#          tag: pkg
+#          default: { string: { text: "{pacman} + {aur} = {pkg}" } }
+#          values:
+#            0: {string: {text: no updates}}
 
 
-declare interval no_update aur_helper pacman_num aur_num pkg_num
+declare interval aur_helper pacman_num aur_num pkg_num
 
 # Error message in STDERR
 _err() {
   printf -- '%s\n' "[$(date +'%Y-%m-%d %H:%M:%S')]: $*" >&2
 }
+
+# Display tags before yambar fetch the updates number
+printf -- '%s\n' "pacman|int|0"
+printf -- '%s\n' "aur|int|0"
+printf -- '%s\n' "pkg|int|0"
+printf -- '%s\n' ""
 
 
 while true; do
@@ -34,11 +51,6 @@ while true; do
   #  "s" seconds / "m" minutes / "h" hours / "d" days 
   interval="1h"
   
-  # Change the message you want when there is 0 update
-  # Leave empty if you want a 0 instead of a string 
-  # (e.g. no_update="")
-  no_update=""
-  
   # Change your aur manager
   aur_helper="paru"
 
@@ -47,33 +59,22 @@ while true; do
 
   if ! hash "${aur_helper}" >/dev/null 2>&1; then
     _err "aur helper not found, change it in the script"
+    exit 1
   else
     aur_num=$("${aur_helper}" -Qmu | wc -l)
   fi
   
   pkg_num=$(( pacman_num + aur_num ))
 
-  # Only display one if there is no update and multiple tags set
-  if [[ "${pacman_num}" == 0 && "${aur_num}" == 0 ]]; then
-    pacman_num="${no_update:-$pacman_num}"
-    aur_num="${no_update:-$aur_num}"
-    pkg_num="${no_update:-$pkg_num}"
-
-    printf -- '%s\n' "pacman|string|"
-    printf -- '%s\n' "aur|string|"
-    printf -- '%s\n' "pkg|string|${pkg_num}"
-    printf -- '%s\n' ""
-  else 
-    printf -- '%s\n' "pacman|string|${pacman_num}"
-    printf -- '%s\n' "aur|string|${aur_num}"
-    printf -- '%s\n' "pkg|string|${pkg_num}"
-    printf -- '%s\n' ""
-  fi
+  printf -- '%s\n' "pacman|int|${pacman_num}"
+  printf -- '%s\n' "aur|int|${aur_num}"
+  printf -- '%s\n' "pkg|int|${pkg_num}"
+  printf -- '%s\n' ""
 
   sleep "${interval}"
 
 done
 
-unset -v interval no_update aur_helper pacman_num aur_num pkg_num
+unset -v interval aur_helper pacman_num aur_num pkg_num
 unset -f _err
 
