b90e01e6e11b8e686ede80cfbcfde9a73c79ba27
novakne <hmachet@zoho.com>
2021-02-24 21:07:17 +0100

    nvim: remove netrw, no need with foot url opener

--- config/nvim/lua/my/options.lua
+++ config/nvim/lua/my/options.lua
@@ -81,6 +81,6 @@ vim.g.loaded_tarPlugin = 1
 vim.g.loaded_tar = 1
 vim.g.loaded_zipPlugin = 1
 vim.g.loaded_zip = 1
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
