267a278a3596bc6a66be963db23ec14ecf808c00
novakane <mail@novakane.xyz>
2021-05-29 21:38:06 +0200

    nvim: update options to follow latest master

--- config/nvim/lua/nk/options.lua
+++ config/nvim/lua/nk/options.lua
@@ -1,66 +1,64 @@
 -- [ Options ]
-local opt = vim.opt
-
 -- [ General ] ( :help nvim-defaults )
 vim.g.mapleader = ' ' -- Change leader key to '<space>'
 vim.g.maplocalleader = ',' -- Change local leader to ','
 
-opt.scrolloff = 5 -- Always 5 lines at the bottom
-opt.backup = false -- No backup file
-opt.undofile = true
-opt.hidden = true -- Enables to switch between unsaved buffers and keep undo history
-opt.splitbelow = true -- Puts new split windows to the bottom of the current
-opt.splitright = true -- Puts new vsplit windows to the right of the current
-opt.equalalways = false
-opt.clipboard = 'unnamedplus' -- Use the clipboard for all operations
-opt.inccommand = 'nosplit' -- Show preview of substitute command
-opt.mouse = 'a' -- Enable mouse
-opt.shortmess = 'filnxtToOFc' -- Don't give |ins-completion-menu| messages.
-opt.pyxversion = 3 -- Use python 3
-opt.selection = 'exclusive' -- Dont select newline in visual mode ( v$ )
-opt.lazyredraw = true -- Do not redraw screen in the middle of a macro
-opt.swapfile = false -- No swap file
-opt.updatetime = 100 -- Smaller updatetime for CursorHold & CursorHoldI
-opt.modelines = 1
--- opt.foldmethod = 'expr'
--- opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.scrolloff = 5 -- Always 5 lines at the bottom
+vim.o.backup = false -- No backup file
+vim.o.undofile = true
+vim.o.hidden = true -- Enables to switch between unsaved buffers and keep undo history
+vim.o.splitbelow = true -- Puts new split windows to the bottom of the current
+vim.o.splitright = true -- Puts new vsplit windows to the right of the current
+vim.o.equalalways = false
+vim.o.clipboard = 'unnamedplus' -- Use the clipboard for all operations
+vim.o.inccommand = 'nosplit' -- Show preview of substitute command
+vim.o.mouse = 'a' -- Enable mouse
+vim.o.shortmess = 'filnxtToOFc' -- Don't give |ins-completion-menu| messages.
+vim.o.pyxversion = 3 -- Use python 3
+vim.o.selection = 'exclusive' -- Dont select newline in visual mode ( v$ )
+vim.o.lazyredraw = true -- Do not redraw screen in the middle of a macro
+vim.o.swapfile = false -- No swap file
+vim.o.updatetime = 100 -- Smaller updatetime for CursorHold & CursorHoldI
+vim.o.modelines = 1
+-- vim.o.foldmethod = 'expr'
+-- vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 
 -- [ Completion ] ( :help 'complete' )
-opt.wildignorecase = true -- Ignore case when completing file and directory
-opt.completeopt = 'menuone,noselect,noinsert' -- options for Insert mode completion ins-completion
-opt.pumheight = 10 -- Maximum number of items to show in the popup menu
+vim.o.wildignorecase = true -- Ignore case when completing file and directory
+vim.o.completeopt = 'menuone,noselect,noinsert' -- options for Insert mode completion ins-completion
+vim.o.pumheight = 10 -- Maximum number of items to show in the popup menu
 
 -- [ Search ]
-opt.hlsearch = false
-opt.ignorecase = true -- Case insensitive searching
-opt.smartcase = true -- Override ignorecase if upper case typed
-opt.grepprg = [[rg --vimgrep --no-heading --smart-case --only-matching]]
-opt.grepformat = [[%f:%l:%c:%m,%f:%l:%m]]
+vim.o.hlsearch = false
+vim.o.ignorecase = true -- Case insensitive searching
+vim.o.smartcase = true -- Override ignorecase if upper case typed
+vim.o.grepprg = [[rg --vimgrep --no-heading --smart-case --only-matching]]
+vim.o.grepformat = [[%f:%l:%c:%m,%f:%l:%m]]
 
 -- [ Ui ]
-opt.termguicolors = true
-opt.showmode = false
-opt.showcmd = false
-opt.cmdheight = 2 -- Better display for messages
-opt.number = true -- Show line number
-opt.relativenumber = true
-opt.linebreak = true -- Don't insert line breaks in the middle of a word
-opt.signcolumn = 'yes:2' -- Always show signcolumns
-opt.textwidth = 100 -- Break line after 100 characters
+vim.o.termguicolors = true
+vim.o.showmode = false
+vim.o.showcmd = false
+vim.o.cmdheight = 2 -- Better display for messages
+vim.o.number = true -- Show line number
+vim.o.relativenumber = true
+vim.o.linebreak = true -- Don't insert line breaks in the middle of a word
+vim.o.signcolumn = 'yes:2' -- Always show signcolumns
+vim.o.textwidth = 100 -- Break line after 100 characters
 
 -- [ Indent ]
 -- Use 4 spaces instead of tab
 -- Copy indent from current line when starting a new line
-opt.cindent = true
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.expandtab = true
+vim.o.cindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
 
 -- [ Wrap ]
-opt.breakindent = true
-opt.showbreak = string.rep(' ', 3) -- Make it so that long lines wrap smartly
-opt.linebreak = true
+vim.o.breakindent = true
+vim.o.showbreak = string.rep(' ', 3) -- Make it so that long lines wrap smartly
+vim.o.linebreak = true
 
 -- Disable some providers and plugins
 vim.g.loaded_node_provider = 0
