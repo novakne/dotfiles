e848143f680b0d90a0d748e3fa4e96a73ae0ebd8
novakane <mail@novakane.xyz>
2021-06-08 15:01:37 +0200

    nvim: cleanup nnn config

--- config/nvim/after/ftplugin/lua.vim
+++ config/nvim/after/ftplugin/lua.vim
@@ -1,6 +1,4 @@
-setlocal shiftwidth=2
-
 " Lua-Formatter
-noremap <buffer> <silent> <LocalLeader>f :LF<CR>
 command! LF !lua-format -i %
+noremap <buffer> <silent> <LocalLeader>f :LF<CR>
 
--- config/nvim/lua/nk/plugins/nnn.lua
+++ config/nvim/lua/nk/plugins/nnn.lua
@@ -1,18 +1,19 @@
 -- [ Nnn ]
-if packer_plugins['nnn.vim'] then
+if packer_plugins['nnn.vim'] and packer_plugins['nnn.vim'].loaded then
 
     local map = require'nk.utils'.bind
 
-    vim.g['nnn#layout'] = {
-        window = { width = 0.5, height = 0.6, highlight = 'Debug' },
+    require'nnn'.setup {
+        set_default_mappings = false,
+        session = 'global',
+        layout = { window = { width = 0.5, height = 0.6, highlight = 'Debug' } },
+        command = 'nnn -eH',
+        action = {
+            ['<c-t>'] = 'tab split',
+            ['<c-h>'] = 'split',
+            ['<c-v>'] = 'vsplit',
+        },
     }
-    vim.g['nnn#set_default_mappings'] = 0
-    vim.g['nnn#action'] = {
-        ['<c-t>'] = 'tab split',
-        ['<c-h>'] = 'split',
-        ['<c-v>'] = 'vsplit',
-    }
-    vim.g['nnn#command'] = 'nnn -eH'
 
     -- Start in the current file's directory
     map('n', '<F6>', '<CMD>NnnPicker %:p:h<CR>')
